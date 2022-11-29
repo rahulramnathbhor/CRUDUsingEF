d2a09124c35c92d756856b46c8560f384915359d
rahul <rahul@LAPTOP-T6G63HRD>
2022-11-29 11:29:49 +0530

    Add project files.

---
+++ CRUDUsingEF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDUsingEF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRUDUsingEF/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUDUsingEF.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "name is Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "company name is Required")]
+        [Display(Name = "company name")]
+        public string CompanyName { get; set; }
+        [Required(ErrorMessage = "price  is Required")]
+        public decimal Price { get; set; }
+    }
+}
